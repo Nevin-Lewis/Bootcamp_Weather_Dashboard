6755c783dd8bdc704dd997e2456eabe5f7bc7102
Nevin Lewis <lewisnevin@utexas.edu>
2022-10-23 07:16:50 -0500

    added forcast function

--- Assets/script.js
+++ Assets/script.js
@@ -7,15 +7,26 @@ var formSubmitHandler = function (event) {
 
 var city_name = city_input.value.trim();
 if (city_name) {
-    getLocationRepo(city_name);
+    getCurrentweatherRepo(city_name);
+    get5dayweatherRepo(city_name);
    city_input.value = '';
   } else {
     alert('Please enter a City name');
   }
 };
 
-var getLocationRepo = function(city_name) {
+var getCurrentweatherRepo = function(city_name) {
     var apiURL= 'https://api.openweathermap.org/data/2.5/weather?q=' + city_name + ',USA&appid=393fe9e5d586a9565ac961c89947a415&units=imperial';
+fetch(apiURL) 
+    .then(function (response) {
+        console.log(response)
+    response.json().then(function (data) {
+        console.log(data);
+    });   
+})
+};
+var get5dayweatherRepo = function(city_name) {
+    var apiURL= 'https://api.openweathermap.org/data/2.5/forecast?q=' + city_name + ',USA&appid=393fe9e5d586a9565ac961c89947a415&units=imperial';
 fetch(apiURL) 
     .then(function (response) {
         console.log(response)
